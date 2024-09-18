600fde3371afbc20a35b27314a3310f68f457e3c
jferrante25 <jf5156@nyu.edu>
2024-09-18 00:28:34 -0400

    change

--- ps-2/ex_2p9_without_for_loop.py
+++ ps-2/ex_2p9_without_for_loop.py
@@ -4,6 +4,8 @@ import timeit
 # without loop method
 #create arrays for each index
 
+#could not figure out how to do without for loop in time
+
 
 i = np.arange(-5,0)
 j = np.arange(-5,0)
@@ -16,10 +18,6 @@ ij=np.array(
 
 #a=sum(np.divide(1,np.sqrt(np.square(i)+np.square(j)+np.square(k)))
 
-a = np.array([1,2,3,4])
-indices = [0, 2] 
-k=a[indices].sum()
-print(k)
 
 
 #print('Madelung constant: ' + str(m))
