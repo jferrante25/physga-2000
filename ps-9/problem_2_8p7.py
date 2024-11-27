c8694455d15cd77e6850a51192ef886eeaf9e5c1
jferrante25 <jf5156@nyu.edu>
2024-11-26 22:49:54 -0500

    update

--- ps-9/problem_2_8p7.py
+++ ps-9/problem_2_8p7.py
@@ -3,51 +3,91 @@ import numpy as np
 import pylab
 import matplotlib.pyplot as plt
 
-def f(r,t):
- g=
- 
+#define constants
+g=9.8
+R=0.08
+C=0.47 
+rho=1.22
+v= 100
+
+
+
+def f(r,t,m):
+
  x = r[0]
  x2 = r[1]
  y = r[2]
  y2 = r[3]
  fx = x2
- fx2 = 
+ fx2 = - np.pi * R**2 * rho * C /(2*m) *x2 *np.sqrt(x2**2 + y2**2) 
  fy = y2
- fy2 = -omega**2 *x**3
- return np.array([fx,fy],float)
+ fy2 = -g - np.pi * R**2 * rho * C /(2*m) *y2 *np.sqrt(x2**2 + y2**2) 
+ return np.array([fx,fx2,fy,fy2],float)
  
 a = 0
-b = 50
+b = 10
 N = 1000
 h = (b-a)/N
 
-tpoints = np.arange(a,b,h)
-xpoints = []
-ypoints = []
 
-#r for part C (initial x value = 1)
-#r = np.array([1.0,0],float)
-#r for part C increased amp (initial x value = 2)
-r = np.array([2.0,0],float)
+tpoints = np.arange(a,b,h)
+xpoints1 = []
+ypoints1 = []
+
+xpoints5 = []
+ypoints5 = []
+
+xpoints10 = []
+ypoints10 = []
+
+
+r = np.array([0.0,v*np.cos(np.pi/6),0.0,v*np.sin(np.pi/6)],float)
 
 for t in tpoints:
- xpoints.append(r[0])
- ypoints.append(r[1])
- k1 = h*f(r,t)
- k2 = h*f(r+0.5*k1, t+0.5*h)
- k3 = h*f(r+0.5*k2, t+0.5*h)
- k4 = h*f(r+k3,t+h)
+ xpoints1.append(r[0])
+ ypoints1.append(r[2])
+ k1 = h*f(r,t,1)
+ k2 = h*f(r+0.5*k1, t+0.5*h,1)
+ k3 = h*f(r+0.5*k2, t+0.5*h,1)
+ k4 = h*f(r+k3,t+h,1)
+ r += (k1+2*k2+2*k3+k4)/6
+ 
+#Part B
+pylab.plot(xpoints1,ypoints1,label='m = 1 kg')
+pylab.xlabel("x (m)")
+pylab.ylabel("y (m)")
+#pylab.show() 
+ 
+
+ 
+# Part C 
+r = np.array([0.0,v*np.cos(np.pi/6),0.0,v*np.sin(np.pi/6)],float)  
+for t in tpoints:
+ xpoints5.append(r[0])
+ ypoints5.append(r[2])
+ k1 = h*f(r,t,5)
+ k2 = h*f(r+0.5*k1, t+0.5*h,5)
+ k3 = h*f(r+0.5*k2, t+0.5*h,5)
+ k4 = h*f(r+k3,t+h,5)
  r += (k1+2*k2+2*k3+k4)/6
 
-#Part C plots 
-#pylab.plot(tpoints,xpoints)
-#pylab.plot(tpoints,ypoints)
-#pylab.xlabel("t")
+ 
+pylab.plot(xpoints5,ypoints5,label='m = 5 kg')
 
-#Part D plots
-pylab.plot(xpoints,ypoints)
-pylab.xlabel("x (m)")
-pylab.ylabel("velocy (m/s)")
+
+r = np.array([0.0,v*np.cos(np.pi/6),0.0,v*np.sin(np.pi/6)],float)  
+for t in tpoints:
+ xpoints10.append(r[0])
+ ypoints10.append(r[2])
+ k1 = h*f(r,t,10)
+ k2 = h*f(r+0.5*k1, t+0.5*h,10)
+ k3 = h*f(r+0.5*k2, t+0.5*h,10)
+ k4 = h*f(r+k3,t+h,10)
+ r += (k1+2*k2+2*k3+k4)/6
+
+ 
+pylab.plot(xpoints10,ypoints10,label='m = 10 kg')
+pylab.legend()
 
 
 pylab.show()
